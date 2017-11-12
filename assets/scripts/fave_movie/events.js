1b514523f8755e1b15db5da0c693ed5bde67b186
deejay94 <dahjoyner12@gmail.com>
2017-11-12 13:11:38 -0500

    Hide messages upon document load

--- assets/scripts/fave_movie/events.js
+++ assets/scripts/fave_movie/events.js
@@ -5,8 +5,8 @@ const api = require('./api.js')
 const ui = require('./ui.js')
 
 $(function () {
-  $('#message').text('New user? Please sign up')
-  $('#message2').text('Registered account? Please sign in')
+  $('#message2').text('New user? Please sign up')
+  $('#message3').text('Registered account? Please sign in')
   $('.sign-up').empty()
   $('.sign-in').empty()
   $('#change-password').hide()
